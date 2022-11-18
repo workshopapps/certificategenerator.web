c94564a2891058f41cf754734f23c891a80e2a0e
Emsun <emsunconcept@gmail.com>
2022-11-18 17:14:04 +0100

    Emsun0211/FAQpage

---
+++ src/Component/FAQ/FAQcomp/FaqModule.jsx
@@ -0,0 +1,23 @@
+export const FaqList = [
+	{
+		id: 1,
+		title: "What are the basic requirements to successfully use Certawi?",
+		body: {
+			item1:
+				"Certawi generates professionally designed certificates for your staff, students etc. You also get to generate certificates in bulk and download in any format of your choice, for free & no sign up required.",
+			item2:
+				"Certawi generates professionally designed certificates for your staff, students etc. You also get to generate certificates in bulk and download in any format of your choice, for free & no sign up required",
+		},
+	},
+
+	{
+		id: 2,
+		title: "What are the basic requirements to successfully use Certawi?",
+		body: {
+			item1:
+				"Certawi generates professionally designed certificates for your staff, students etc. You also get to generate certificates in bulk and download in any format of your choice, for free & no sign up required.",
+			item2:
+				"Certawi generates professionally designed certificates for your staff, students etc. You also get to generate certificates in bulk and download in any format of your choice, for free & no sign up required",
+		},
+	},
+];
--- src/Component/FAQ/index.jsx
+++ src/Component/FAQ/index.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { FaqContainer } from "./FAQcomp/FaqContainer";
 // import faqStyle from "./faq.module.scss";
 import { FaqHeader } from "./FAQcomp/FaqHeader";
+
 export const FAQ = () => {
 	return (
 		<>
