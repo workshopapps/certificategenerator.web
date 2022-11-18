09072c39a21a4aeb25ee3565eaa7e07a2e706423
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-18 18:00:58 +0100

    Rendered Hero component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./Component/Layout";
+import Hero from "./Component/Hero";
 // import Navbar from './Component/Navbar';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Layout />}>
 					{/* <Route path="/genetrate" element={<Generate />} /> */}
+					<Route path="/" element={<Hero />} />
 				</Route>
 			</Routes>
 		</div>
