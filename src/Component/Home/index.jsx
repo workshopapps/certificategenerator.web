13ea60e28eb290d1cb3361608245524dfd8e8dbe
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-18 22:15:32 +0100

    Rendered bulk-step section

--- src/Component/Home/index.jsx
+++ src/Component/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../Hero";
 import Certificate from "../Certificate";
+import BulkStep from "../BulkStep";
 
 export default function Home () {
     return (
         <>
           <Hero />
           <Certificate />
+          <BulkStep />
         </>
     )
 }
