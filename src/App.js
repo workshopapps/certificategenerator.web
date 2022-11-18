e0637981a91277d8ffe3ffd63323e35bfa5cd0ae
alvyynm <alvyynm@gmail.com>
2022-11-19 00:01:17 +0300

    chore: final commit 🚀

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Layout from "./Component/Layout";
 // import Navbar from './Component/Navbar';
 
 function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					{/* <Route path="/genetrate" element={<Generate />} /> */}
-				</Route>
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route path="/genetrate" element={<Generate />} /> */}
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
