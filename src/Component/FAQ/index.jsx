d369abf3ef7b5118891327f43406cc3cb93821ef
Emsun <emsunconcept@gmail.com>
2022-11-18 07:18:03 +0100

    added the faq components

--- src/Component/FAQ/index.jsx
+++ src/Component/FAQ/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { FaqContainer } from "./FAQcomp/FaqContainer";
 // import faqStyle from "./faq.module.scss";
 import { FaqHeader } from "./FAQcomp/FaqHeader";
 export const FAQ = () => {
-	return <FaqHeader />;
+	return (
+		<>
+			<FaqHeader />
+			<FaqContainer />
+		</>
+	);
 };
