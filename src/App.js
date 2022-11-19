7dc9180cd7ddaecffe6d843faab133384c5d3aa8
jargohillzz <chukwunonsougwu14@gmail.com>
2022-11-19 14:40:59 +0100

    FRO-26: navbar component commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./Component/Layout";
+import Navbar from "./Component/Navbar";
 // import Navbar from './Component/Navbar';
 
 function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					{/* <Route path="/genetrate" element={<Generate />} /> */}
-				</Route>
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route path="/genetrate" element={<Generate />} /> */}
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
