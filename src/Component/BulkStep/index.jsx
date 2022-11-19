56969ba16fd3f7ac6485f37f6d7861a8eac65e2b
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-19 14:46:06 +0100

    Started implementing responsiveness

--- src/Component/BulkStep/index.jsx
+++ src/Component/BulkStep/index.jsx
@@ -9,15 +9,21 @@ export default function BulkStep () {
     return (
         <div className="bulk">
           <p className="heading sora">Create bulk Certificates in <span className="emphasized">3 easy steps</span></p>
-          <div className="flex space center">
-            <img src={example} alt="bulk-example" style={{cursor: 'pointer'}}/>
+          <div className="flex space center items-center">
+            <img src={example} alt="bulk-example" style={{cursor: 'pointer'}} className="example"/>
             <div className="text-left work-sans">
-                <img src={one} alt="one" />
-                <p className="steps">Select certificate template</p>
-                <img src={two} alt="two" />
-                <p className="steps">Upload a CSV file</p>
-                <img src={three} alt="three" />
-                <p className="steps">Generate bulk certificates</p>
+                <div className="process">
+                  <img src={one} alt="one" />
+                  <p className="steps">Select certificate template</p>
+                </div>
+                <div className="process">
+                  <img src={two} alt="two" />
+                  <p className="steps">Upload a CSV file</p>
+                </div>
+                <div className="process">
+                  <img src={three} alt="three" />
+                  <p className="steps">Generate bulk certificates</p>
+                </div>
             </div>
           </div>
           <button className="bulk-button work-sans">Create Bulk Certificates</button>
