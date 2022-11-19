3881c8187ad53bef3487706166fcc29f20885416
Gbolahan <hormorgborlahan@yahoo.com>
2022-11-19 20:36:48 +0100

    done with color scheme

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Choice from "./Component/Choice(single or bulk)";
 import Layout from "./Component/Layout";
+import Modify from "./Component/Modify";
+
 // import Navbar from './Component/Navbar';
 
 function App() {
@@ -10,7 +12,8 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Layout />}>
 					{/* <Route path="/genetrate" element={<Generate />} /> */}
-					<Route path="/choice" element={<Choice />} />
+					<Route path="choice" element={<Choice />}></Route>
+					<Route path="modify" element={<Modify />} />
 				</Route>
 			</Routes>
 		</div>
