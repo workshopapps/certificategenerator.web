ff026958d68652a1ad0099845f1b165726c97d59
Amina <aminaumohammed@gmail.com>
2022-11-19 11:23:11 +0100

    About Us

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./Component/Layout";
+import AboutUs from "./Component/AboutUs";
+
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<Layout />}>
+
 					{/* <Route path='/genetrate' element={<Generate/>}/> */}
 				</Route>
+				<Route path="/aboutUs" element={<AboutUs />}>
+
+					{/* <Route path='/genetrate' element={<Generate/>}/> */}
+				</Route>
+
 			</Routes>
 		</div>
 	);
--- src/Component/Layout/index.jsx
+++ src/Component/Layout/index.jsx
@@ -1,7 +1,8 @@
 import "./layout.style.scss";
-
 import React from "react";
 import { Outlet } from "react-router-dom";
+
+
 const Layout = () => {
 	return (
 		<div>
