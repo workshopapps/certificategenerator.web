b7fc3cbf6e438b02f07db9241eb8344beaee9878
Emsun <emsunconcept@gmail.com>
2022-11-19 13:28:37 +0100

    Emsun0211/Added the FAQ route in the APP.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Team from "./Component/Team";
 import Layout from "./Component/Layout";
+import { FAQ } from "./Component/FAQ/index";
 // import Navbar from './Component/Navbar';
 
 function App() {
