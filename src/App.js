f8277e11abdd0617173a9d246edfd0fc0fbcd4ab
Tolulope Babatunde <babsbright69@gmail.com>
2022-11-19 02:24:08 +0100

    worked on the single certificate preview page, bulk certificate preview page and edit bulk page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./Component/Layout";
+
+import SinglePreview from "./Component/SinglePreview";
+import BulkPreview from "./Component/BulkPreview";
+import EditBulk from "./Component/EditBulk";
 // import Navbar from './Component/Navbar';
 
 function App() {
 	return (
 		<div className="App">
+			
+		
+		
 			<Routes>
-				<Route path="/" element={<Layout />}>
-					{/* <Route path="/genetrate" element={<Generate />} /> */}
+				<Route exact path="/" element={<Layout />}>
+
 				</Route>
+				
+				<Route path="/single_preview" element={<SinglePreview />} />
+				<Route path="/bulk_preview" element={<BulkPreview />} />
+
+				
+				<Route path="/edit_bulk" element={<EditBulk />} />
+
+				
 			</Routes>
 		</div>
 	);
--- src/Component/Layout/index.jsx
+++ src/Component/Layout/index.jsx
@@ -1,11 +1,12 @@
 import "./layout.style.scss";
-
+import SinglePreview from "../SinglePreview";
 import React from "react";
 import { Outlet } from "react-router-dom";
 const Layout = () => {
 	return (
 		<div>
 			Nav
+			<SinglePreview />
 			<Outlet />
 			Footer
 		</div>
