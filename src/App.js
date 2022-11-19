4e4193f587d5622cc26f138bc3494d22eab8244c
Adepejustephen <adepejuifeoluwa97@gmail.com>
2022-11-19 22:44:28 +0100

    FRO-28: create contact info

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
 import { Route, Routes } from "react-router-dom";
-import Dashboard from './Component/Dashboard';
-import Team from './Component/Team';
-import Error from './Component/Error';
-import Terms from './Component/Terms';
-import Layout from './Component/Layout';
-import SinglePreview from './Component/SinglePreview';
-import BulkPreview from './Component/BulkPreview';
-import EditBulk from './Component/EditBulk';
-import { FAQ } from './Component/FAQ/index';
-import Navbar from './Component/Navbar';
-import Templates from './Component/Templates';
-import AboutUs from './Component/AboutUs';
-import Comingsoon from './Component/Coming/Comingsoon';
+import Dashboard from "./Component/Dashboard";
+import Team from "./Component/Team";
+import Error from "./Component/Error";
+import Terms from "./Component/Terms";
+import Layout from "./Component/Layout";
+import SinglePreview from "./Component/SinglePreview";
+import BulkPreview from "./Component/BulkPreview";
+import EditBulk from "./Component/EditBulk";
+import { FAQ } from "./Component/FAQ/index";
+import Navbar from "./Component/Navbar";
+import Templates from "./Component/Templates";
+import AboutUs from "./Component/AboutUs";
+import Comingsoon from "./Component/Coming/Comingsoon";
 import { ContactUs } from "./pages";
-import './App.css';
-
-
-
-
-
+import "./Style/App.scss";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className='App'>
+      <div className="App">
         <Routes>
-          <Route path='/' element={<Layout />} />
-          <Route path='/comingsoon' element={<Comingsoon />} />
-          <Route path='/team' element={<Team />} />
-          <Route path='/terms' element={<Terms />} />
-          <Route path='/templates' element={<Templates />} />
-          <Route path='/FAQ' element={<FAQ />} />
-          <Route path='/aboutUs' element={<AboutUs />} />
-          <Route path='/single_preview' element={<SinglePreview />} />
-          <Route path='/bulk_preview' element={<BulkPreview />} />
-          <Route path='/edit_bulk' element={<EditBulk />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-				  <Route path='*' element={<Error />} />
-				  <Route path="/contact" element={<ContactUs />}/>
+          <Route path="/" element={<Layout />} />
+          <Route path="/comingsoon" element={<Comingsoon />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/templates" element={<Templates />} />
+          <Route path="/FAQ" element={<FAQ />} />
+          <Route path="/aboutUs" element={<AboutUs />} />
+          <Route path="/single_preview" element={<SinglePreview />} />
+          <Route path="/bulk_preview" element={<BulkPreview />} />
+          <Route path="/edit_bulk" element={<EditBulk />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Error />} />
+          <Route path="/contact" element={<ContactUs />} />
         </Routes>
       </div>
     </>
--- src/pages/contactUs/index.jsx
+++ src/pages/contactUs/index.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import { ContactChatIcon } from '../../assests'
+import React from "react";
+import { ContactChatIcon, ContactLocationIcon, ContactPhoneIcon } from "../../assets";
+import "./contact.scss";
 
 const ContactUs = () => {
   return (
-      <div className="contact">
-          <div className="contact__content">
-              {/* Contact Info */}
-              
-              <div className="contact__info">
-                  <div className="contact__info--item">
-                      <ContactChatIcon/>
-                  </div>  
-              </div>
-          </div>
-    </div>
-  )
-}
+    <div className="contact">
+      <div className="contact__content">
+        {/* Contact Info */}
 
-export default ContactUs
+        <div className="contact__info">
+          <div className="contact__info--item">
+            <ContactChatIcon />
+            <div className="contact__info--itemText">
+              <h4 className="contact__info--key">chat with customer suport</h4>
+              <p className="contact__info--address">chat with sales team</p>
+              <span>support@certawi.com</span>
+            </div>
+          </div>
+          <div className="contact__info--item">
+            <ContactLocationIcon />
+            <div className="contact__info--itemText">
+              <h4 className="contact__info--key">visit us</h4>
+              <p className="contact__info--address">visit our office</p>
+              <span>121 vincent str., lagos</span>
+            </div>
+          </div>
+          <div className="contact__info--item">
+            <ContactPhoneIcon />
+            <div className="contact__info--itemText">
+              <h4 className="contact__info--key">call us</h4>
+              <p className="contact__info--address">mon-Fri from 8am-6pm</p>
+              <span>+234900-000-000</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
