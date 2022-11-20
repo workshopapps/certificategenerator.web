2fd6815652342bb22a0fe0815957e035e3d71280
Tolulope Babatunde <babsbright69@gmail.com>
2022-11-20 20:11:30 +0100

    added styling and changed some features

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,42 +1,55 @@
+import "./Style/App.scss";
+import Navbar from "./Component/Navbar";
+import { Route, Routes } from "react-router-dom";
+import {
+  AboutUs,
+  BulkPreview,
+  BulkStep,
+  Choice,
+  ComingSoon,
+  Dashboard,
+  EditBulk,
+  Error,
+  FAQ,
+  Layout,
+  Modify,
+  SinglePreview,
+  Team,
+  Templates,
+  Terms,
+} from "./pages";
+import Footer from "./Component/Footer";
 
-import './Style/App.scss';
-import Navbar from './Component/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import { AboutUs, BulkPreview, BulkStep, Choice,ComingSoon, Dashboard, EditBulk, Error, FAQ, Layout, Modify, SinglePreview, Team, Templates, Terms } from './pages';
-import Footer from './Component/Footer';
-
-import Home from './pages/Home'
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className='App'>
+      <div className="App">
         <Routes>
-          <Route path='/' element={<Layout />} >
-          <Route index element={<Home />} />
-          <Route path='/comingsoon' element={<ComingSoon />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/templates' element={<Templates />} />
-          <Route path="choice" element={<Choice />}/>
-					<Route path="modify" element={<Modify />} />
-          <Route path='/team' element={<Team />} />
-          <Route path='/terms' element={<Terms />} />
-          <Route path='/single_preview' element={<SinglePreview />} />
-          <Route path='/aboutUs' element={<AboutUs />} />
-          <Route path='/FAQ' element={<FAQ />} />
-          <Route path='/bulk_preview' element={<BulkPreview />} />
-          <Route path='/bulk_step' element={<BulkStep />} />
-          <Route path='/edit_bulk' element={<EditBulk />} />
-         
-            <Route path='*' element={<Error />} />
-            </Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/comingsoon" element={<ComingSoon />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/templates" element={<Templates />} />
+            <Route path="choice" element={<Choice />} />
+            <Route path="modify" element={<Modify />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/single_preview" element={<SinglePreview />} />
+            <Route path="/aboutUs" element={<AboutUs />} />
+            <Route path="/FAQ" element={<FAQ />} />
+            <Route path="/bulk_preview" element={<BulkPreview />} />
+            <Route path="/bulk_step" element={<BulkStep />} />
+            <Route path="/edit_bulk" element={<EditBulk />} />
+          </Route>
+          <Route path="*" element={<Error />} />
         </Routes>
       </div>
       {/* <Footer /> */}
     </>
   );
-
 }
 
 export default App;
