3a03067421d4dab3f33d915b651bb1cd8a201831
ABDULMALIK-ADULLAHI <aabdullahi@systemspecs.com.ng>
2022-11-20 22:49:50 +0100

    adjusting

---
+++ Frontend/src/pages/Checkout/components/CheckoutMain.jsx
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../pagescss/CheckoutPage.css'
+import '../pagesresponsivecss/CheckoutPageResponsive.css'
+import CheckoutMainLeft from './CheckoutMainLeft'
+import CheckoutMainRight from './CheckoutMainRight'
+
+function CheckoutMain() {
+  return (
+    <div className="container-fluid" id='CheckoutMain-main-container'>
+        
+        <h4 id='CheckoutMain-text'>Checkout</h4>
+
+        <div className="row" id='CheckoutMain-row'>
+
+            <CheckoutMainLeft />
+
+            <CheckoutMainRight />
+
+        </div>
+
+    </div>
+  )
+}
+
+export default CheckoutMain
+
