4609fee89cf6be665a8a3eccd2cbedfe9e4a0835
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2022-11-21 16:51:11 +0100

    chore: testing

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const fileUpload = require('express-fileupload');
--- Backend/middleware/authValidators.js
+++ Backend/middleware/authValidators.js
@@ -1,4 +1,4 @@
-const {body, check} = require("express-validator")
+const {body, check} = require("express-validator");
 
 const validateSignUp = check("password") ? [body("email").trim().isEmail().withMessage("Please enter a valid email!").normalizeEmail(), body("password").trim().not().isEmpty()] : [];
 // exports.validateSignUp = [];
