5a70e464182355a1692856a03692ac434b28d362
Abdulkabir sultan <88564842+abdulkabirsultan@users.noreply.github.com>
2022-11-21 21:07:02 +0100

    Update dbConn.js

--- Backend/utils/dbConn.js
+++ Backend/utils/dbConn.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/tesrtDB', {
+    await mongoose.connect(process.env.MONGODB_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
