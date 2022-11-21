7bbd4c37b6fcfcb91036299cb64eee4014b7a352
Christian Ezeanyaeche <christianezeanyaeche@gmail.com>
2022-11-21 18:20:58 +0100

    fix(BE-4): changed req.user.id to req.user.userId

--- Backend/controllers/downloadController.js
+++ Backend/controllers/downloadController.js
@@ -1,16 +1,19 @@
 const userModel = require('../models/userModel')
 
 // @desc    downloads certifcate records from User.records array
-// @route   POST /api/download
+// @route   POST( or redirect with 307) /api/download
 // @access  Private
-// @ data = collectionID
+// @data = collectionID
 exports.getUserData = async (req, res, next) => {
     try {
         userModel.findOne({
             // from authentication token
-            _id : req.user.id
+            _id : req.user.userId
         }, (err, data) => {
             try {
+                if(err) {
+                    return next(err)
+                }
                 res.send(data.records.filter(item => item.collectionID === req.body.collectionID))
             } catch (error) {
                 next(error)
