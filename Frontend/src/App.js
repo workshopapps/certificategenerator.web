1512245e3fa64ffe0ed8efae56186b383dc10d77
Luqman07 <luqmanadeniyi97@gmail.com>
2022-11-23 10:41:28 +0100

    fix: changed the color code of the table to the style guide code

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -88,6 +88,7 @@ function App() {
             <Route path="/upload" element={<UploadCSV />} />
 
             <Route path="/privacy" element={<Privacy />} />
+
           </Route>
           <Route path="*" element={<Error />} />
         </Routes>
