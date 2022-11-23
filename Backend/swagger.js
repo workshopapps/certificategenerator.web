9f0e47b86d9862e0f1726d2d691fb417c3c8ee0b
Iyobosa Omoruyi <omosiyobo@gmail.com>
2022-11-23 12:21:05 +0100

    configuration

--- Backend/swagger.js
+++ Backend/swagger.js
@@ -1,6 +1,18 @@
 const swaggerAutogen = require('swagger-autogen')()
 
-const outputFile = './swagger_output.json'
-const endpointsFiles = ['./index.js']
+const doc = {
+  info: {
+    title: 'Certificate Generator API',
+    description: "A certificate generator api with other features",
+  },
+  host: 'certify-api.onrender.com',
+  basePath: "/",
+  schemes: ['https'],
+};
 
-swaggerAutogen(outputFile, endpointsFiles)
+const outputFile = './swagger_output.json'
+const endpointsFiles = [
+  './index.js'
+]
+
+swaggerAutogen(outputFile, endpointsFiles, doc)
