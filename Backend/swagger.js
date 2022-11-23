dd51d27910bdc8a6ed7093b7574ee282cebfe184
Iyobosa Omoruyi <omosiyobo@gmail.com>
2022-11-23 12:48:39 +0100

    configuration fixed

--- Backend/swagger.js
+++ Backend/swagger.js
@@ -1,5 +1,15 @@
 const swaggerAutogen = require('swagger-autogen')()
 
+const doc = {
+  info: {
+    version: '1.0.0',
+    title: 'CERTIFY API',
+    description: "",
+  },
+  host: 'certify-api.onrender.com',
+  schemes: ['https'],
+};
+
 const outputFile = './swagger_output.json'
 const endpointsFiles = [
   './index.js'
