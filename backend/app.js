4de57e94717c12a9fc7d89a3fc2d05ea97182df4
codak77 <seanchi77@gmail.com>
2022-11-25 10:07:37 +0100

    .env

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(fileUpload());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Welcome to HNG-Certificate Api");
 });
 
