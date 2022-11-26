becfb36205b4eddbd0466b7794a0d68aab685006
yusuf abdulsamad <yusufabdulsamad93@gmail.com>
2022-11-26 02:22:09 +0100

    https://linear.app/team-headlight/issue/BE-21/be-profile-page

--- Backend/models/profileModel.js
+++ Backend/models/profileModel.js
@@ -2,38 +2,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const ProfileSchema = new mongoose.Schema({
-<<<<<<< HEAD
-   user:{
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User'
-    },
-   avatar: {
-      type: String,
-   },
-   name: {
-      type: String,
-      required: [true, 'Must provide your name!']
-   },
-   job: {
-      type: String,
-      required: [true, 'Must provide a job!']
-   },
-   location: {
-      type: String,
-      required: [true, 'Must provide a location']
-   },
-   email: {
-      type: String,
-      required: [true, 'Must provide a valid email address!'],
-      unique: true
-   },
-   phoneNumber: {
-      type: String,
-      required: [true, 'Must provide a valid phone number!'],
-=======
   user: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     required: true,
     ref: "User",
   },
@@ -62,6 +32,5 @@ const ProfileSchema = new mongoose.Schema({
     required: [true, "Must provide a valid phone number!"],
   },
 });
->>>>>>> 86ad619ae164b2daf428c9dd9ef3f438e9e60dca
 
 module.exports = mongoose.model("Profile", ProfileSchema);
