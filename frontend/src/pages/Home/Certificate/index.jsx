e98eaa495772957b08965a379dc33f66ca581bb7
Samuel <Samueladeyemi2006@gmail.com>
2022-11-30 17:34:08 +0100

    fix: consistent toggling for hompage certificate tab done

--- frontend/src/pages/Home/Certificate/index.jsx
+++ frontend/src/pages/Home/Certificate/index.jsx
@@ -34,13 +34,13 @@ export default function Certificate ({
 
           {bulkCertificate ? 
           <div className="flex justify-between mode">
-            <button className="select" style={{color: '#222222', backgroundColor: '#ffffff'}}  onClick={() => {setBulkCertificate(false)}}>Single <span className="mobile-none">Certificate</span></button>
+            <button className="select" style={{color: '#222222', backgroundColor: '#ffffff', transition: '300ms ease-in', fontWeight: '400'}}  onClick={() => {setBulkCertificate(false)}}>Single <span className="mobile-none">Certificate</span></button>
             <button className="select" onClick={() => {setBulkCertificate(true)}}>Bulk <span className="mobile-none">Certificate</span></button>
           </div>
           :
           <div className="flex justify-between mode">
             <button className="select"  onClick={() => {setBulkCertificate(false)}}>Single <span className="mobile-none">Certificate</span></button>
-            <button className="select" style={{color: '#222222', backgroundColor: '#ffffff'}} onClick={() => {setBulkCertificate(true)}}>Bulk <span className="mobile-none">Certificate</span></button>
+            <button className="select" style={{color: '#222222', backgroundColor: '#ffffff', transition: '300ms ease-in', fontWeight: '400'}} onClick={() => {setBulkCertificate(true)}}>Bulk <span className="mobile-none">Certificate</span></button>
           </div>
         }
           
