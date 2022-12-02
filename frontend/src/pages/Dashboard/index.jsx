1f4986f5127401fd64ca6c0fe2978e76d47accfc
Gbolahan Oladeji <hormorgborlahan@yahoo.com>
2022-12-02 07:54:46 +0100

    uniform button on commingsoon page

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -3,6 +3,7 @@ import "./dashboard.style.scss";
 import profilePic from "../../assets/images/Ellipse4.png";
 import Card from "./Card";
 import { cardData, nullDataIcon, actionIcon } from "./utils";
+import Button from "../../Component/button";
 
 const Dashboard = () => {
   const [data, setData] = useState([]);
@@ -19,34 +20,34 @@ const Dashboard = () => {
 
       let requestOptions = {
         method: "GET",
-        headers: myHeaders,
+        headers: myHeaders
       };
 
       fetch("https://certify-api.onrender.com/api/certificates", requestOptions)
-        .then((response) => response.json())
-        .then((result) => setData(result))
-        .catch((error) => console.log("error", error));
+        .then(response => response.json())
+        .then(result => setData(result))
+        .catch(error => console.log("error", error));
 
       fetch(
         "https://certify-api.onrender.com/api/certificates/issuedCertificates",
         requestOptions
       )
-        .then((response) => response.json())
-        .then((result) => {
+        .then(response => response.json())
+        .then(result => {
           setIssuedCert(
-            issuedCert.map((item) =>
+            issuedCert.map(item =>
               item.title === "Total Number Issued"
                 ? { ...item, count: result.issuedCertificates }
                 : item
             )
           );
         })
-        .catch((error) => console.log("error", error));
+        .catch(error => console.log("error", error));
     };
     fetchData();
   }, []);
 
-  const dataCheck = issuedCert.filter((item) => item.count !== 0);
+  const dataCheck = issuedCert.filter(item => item.count !== 0);
 
   return (
     <>
@@ -81,7 +82,7 @@ const Dashboard = () => {
             <p>CERTIFICATE DASHBOARD</p>
             {dataCheck.length > 0 ? (
               <div>
-                <button className="">Create New Certificate</button>
+                <Button className="">Create New Certificate</Button>
               </div>
             ) : null}
           </div>
