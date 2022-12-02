3b0fa09e55999ce502cd20ac6dbc910b7c638f67
Samuel <Samueladeyemi2006@gmail.com>
2022-12-02 14:28:36 +0100

    added transition to tab

--- frontend/src/pages/Home/Certificate/index.jsx
+++ frontend/src/pages/Home/Certificate/index.jsx
@@ -38,13 +38,13 @@ export default function Certificate ({
 
           {bulkCertificate ? 
           <div className="flex justify-between mode">
-            <button className="select" style={{color: '#222222', backgroundColor: '#ffffff'}}  onClick={() => {setBulkCertificate(false)}}>Single <span className="mobile-none">Certificate</span></button>
+            <button className="select" style={{color: '#222222', backgroundColor: '#ffffff', transition: '300ms ease-in'}}  onClick={() => {setBulkCertificate(false)}}>Single <span className="mobile-none">Certificate</span></button>
             <button className="select" onClick={() => {setBulkCertificate(true)}}>Bulk <span className="mobile-none">Certificate</span></button>
           </div>
           :
           <div className="flex justify-between mode">
             <button className="select"  onClick={() => {setBulkCertificate(false)}}>Single <span className="mobile-none">Certificate</span></button>
-            <button className="select" style={{color: '#222222', backgroundColor: '#ffffff'}} onClick={() => {setBulkCertificate(true)}}>Bulk <span className="mobile-none">Certificate</span></button>
+            <button className="select" style={{color: '#222222', backgroundColor: '#ffffff', transition: '300ms ease-in'}} onClick={() => {setBulkCertificate(true)}}>Bulk <span className="mobile-none">Certificate</span></button>
           </div>
         }
           
