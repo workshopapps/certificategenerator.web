3f8889349357a9ea69c4c360511fe1584ac20b0c
Hillary Odo <Hillary.Odo@mainone.net>
2022-12-03 19:11:02 +0100

    fix: branch created to fix merging conflicts

--- frontend/src/pages/UploadCSV/index.jsx
+++ frontend/src/pages/UploadCSV/index.jsx
@@ -52,10 +52,10 @@ const UploadCSV = ({ setFile }) => {
       </div>
       
       {/* <h1>Your certificate is almost ready!</h1> */}
-      <div className="certificatePreview">
+      {/* <div className="certificatePreview">
         <img src={Certificate} alt="certificate preview" />
-      </div>
-      <h2>Upload your csv file here in the format below</h2>
+      </div> */}
+      <h2>Upload your csv file in the format below</h2>
       <div className="CSVSample">
         <img src={CSVSample} alt="CSV sample" />
       </div>
@@ -67,31 +67,31 @@ const UploadCSV = ({ setFile }) => {
         //   onDragOver={handleDrag}
         //   onDrop={handleDrop}
       >
-        <i>
-          <img src={UploadVector} alt="upload icon" />
-        </i>
-        <span>
-          Drag and drop your CSV file here
-          <br /> or
-          <label htmlFor="uploadCSV" className="fileUpload CSVupload">
-            <input
-              type="file"
-              id="uploadCSV"
-              name="uploadCSV"
-              accept=".csv"
-              className="box"
-              onChange={e=>validateInput(e)}
-            />
-            Browse files
-          </label>
-        </span>  
+        <div className="dragboxContainer">
+          <i>
+            <img src={UploadVector} alt="upload icon" />
+          </i>
+          <span >
+            Drag and drop your CSV file here
+            <br /> or <br />
+            <label htmlFor="uploadCSV" className="fileUpload CSVupload">
+              <input
+                type="file"
+                id="uploadCSV"
+                name="uploadCSV"
+                accept=".csv"
+                className="box"
+                onChange={e=>validateInput(e)}
+              />
+            </label>
+          </span> 
+        </div> 
         {errorFile && <div className="messagecsv">Invalid file!! submit only csv files</div>}   
-        <div className="Submitcsv" onClick={validateInput}>Submit file</div>
+        <div className="Submitcsv" onClick={validateInput}>Submit CSV</div>
         
       </div>
       {/* <button className='btn btnLight'>Generate Certificate</button> */}
-      <button style={{ display: "none" }}>Upload</button>
-      <div>
+      {/* <div>
         <h2>Even More Template for You</h2>
         <div className="moreTemplate">
           <img src={Template1} alt="Template1" />
@@ -101,7 +101,7 @@ const UploadCSV = ({ setFile }) => {
         <Button className="btn btnLight" style={{ margin: "1em auto" }}>
           Explore More Template
         </Button>
-      </div>
+      </div> */}
     </div>
   );
 };
