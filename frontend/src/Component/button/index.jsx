f24b01e1d0fb5a026cc04987c29ffbfcdc4422dc
Adepejustephen <adepejuifeoluwa97@gmail.com>
2022-12-03 19:13:44 +0100

    add validations to contact-us inputfields

--- frontend/src/Component/button/index.jsx
+++ frontend/src/Component/button/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './button.scss'
 
-const Button = ({children, name, width, className, onClick}) => {
+const Button = ({children, name, width, className, onClick, disabled}) => {
   return (
-      <button className={`btn ${className} `} style={{
-        width: width
-    }} onClick={onClick}>{children} {name}</button>
+    <button className={`btn ${className} `} style={{
+      width: width
+    }} onClick={onClick} disabled={disabled}>{children} {name}</button>
   )
 }
 
