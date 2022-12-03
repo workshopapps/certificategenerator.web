ca3cdb8880b051ab8397a8e9524ec306ff8ea9c1
Isaac Ubani <32121772+ubaniIsaac@users.noreply.github.com>
2022-12-03 15:05:32 +0100

    Delete backend directory

--- backend/middleware/authValidators.js
+++
@@ -1,5 +0,0 @@
-const {body, check} = require("express-validator");
-
-const validateSignUp = check("password") ? [body("email").trim().isEmail().withMessage("Please enter a valid email!").normalizeEmail(), body("password").trim().not().isEmpty()] : [];
-// exports.validateSignUp = [];
-module.exports = validateSignUp;
--- backend/routes/downloadRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/downloadController')
-
-router.post("/", controller.getUserData)
-
-module.exports = router
