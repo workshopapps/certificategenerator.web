e65df9dd060deda4b3d55ea4f9a876b348273d12
Niyi <cointerior15@gmail.com>
2022-12-04 12:10:47 +0100

    fix(BE-61): fixed delete all user certificates

--- backend/controllers/userCertificateController.js
+++ backend/controllers/userCertificateController.js
@@ -1,4 +1,5 @@
 const User = require("../models/certificateModel");
+const UserBio = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const csvToJson = require("csvtojson");
 const { v4 } = require("uuid");
@@ -88,7 +89,7 @@ const deleteUserCertificates = async(req, res) => {
 
   const token = auth.split(" ")[1];
   const { userId } = jwt.decode(token);
-  const user = await User.findById(userId).exec();
+  const user = await UserBio.findById(userId).exec();
   if (!user) return res.status(404).json({ message: "user not found" });
 
   await User.deleteMany({ userId })
