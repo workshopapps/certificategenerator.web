7bbfc7fb9f49b071dcc4bc7ac49ffbe39a7cb19b
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-12-04 16:27:24 +0100

    fix: Fix bug

--- frontend/src/pages/Preview/index.jsx
+++ frontend/src/pages/Preview/index.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './preview.scss';
+import UploadCSV from '../UploadCSV';
 import SinglePreview from '../SinglePreview';
-import BulkPreview from '../BulkPreview';
 
 export default function Preview({  
   logo,
@@ -9,8 +9,7 @@ export default function Preview({
   awardeeName,
   message,
   issuedBy,
-  issueDate,
-  certificatesData }) {
+  issueDate }) {
 
   const [bulkCertificate, setBulkCertificate] = useState(false);
 
@@ -28,7 +27,7 @@ export default function Preview({
           </div>
         }
           {bulkCertificate ?  (
-            <BulkPreview certificatesData={certificatesData} />
+            <UploadCSV />
           ) : 
            
              <SinglePreview   
