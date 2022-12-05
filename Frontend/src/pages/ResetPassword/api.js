8ce7a622b44b8b19e6479cb8e696c6ed452af922
Samuel <Samueladeyemi2006@gmail.com>
2022-12-05 18:44:32 +0100

    ..

---
+++ Frontend/src/pages/ResetPassword/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseURL = "https://certify-api.onrender.com/api/auth/";
+
+const axiosInstance = axios.create({
+  baseURL,
+  headers: { "Content-Type": "application/json" }
+});
+
+export const ResetPassword = userData => {
+  return axiosInstance.post(`/changepassword/:userId/:token`, userData);
+};
--- frontend/src/pages/ResetPassword/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword/ResetPassword.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useNavigate, useParams,} from "react-router-dom";
 import Input from "../../Component/Input";
 import Layout from "./ResetLayout";
+import Swal from "sweetalert2";
 
 const ResetPassword = () => { 
   const navigate = useNavigate();
@@ -12,6 +13,19 @@ const ResetPassword = () => {
   })
   const { userId, token } = useParams();
 
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: toast => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    }
+  });
+  
+
   
   const handleChange = (e) =>{
     setFormData({
@@ -19,19 +33,46 @@ const ResetPassword = () => {
          [e.target.name]: e.target.value
     })
   }
+    async function resetPassword({...formData}) {
+    return fetch(`https://certify-api.onrender.com/api/auth/changepassword/${userId}/${token}`, {
+      method: "POST",
+      //  //mode: 'no-cors',
+      // credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        // "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Methods": "POST",
+        // "Access-Control-Allow-Headers": "Content-Type, Authorization"
+      },
+      body: JSON.stringify({...formData})
+      
+    });
+  }
   const handleSubmit = async (e) =>{
     e.preventDefault()
-    if(formData.newPassword !== formData.confirmPassword){
+    try {
+       if(formData.newPassword !== formData.confirmPassword){
       alert('password do not match')
     }else{
-      await  axios.post(`https://certify-api.onrender.com/api/auth/changepassword/${userId}/${token}`, {...formData})
-      .then((response) => {
+       const response = await resetPassword({...formData});
+      //  const data = await response.json();
+      // await axios.post(`https://certify-api.onrender.com/api/auth/changepassword/${userId}/${token}`, {...formData})
+      // .then((response) => {
+         if (response.status === 200 || response.status === 201) {
+              Toast.fire({
+                icon: "success",
+                title: "password reset link sent to your email account"
+              });
             navigate('/fff5')
+            console.log(response);
             console.log(response.data.message);
-          }).catch(err =>{
-            console.error(err)
-          })
+         }
+          // })
     }
+    } catch (error) {
+      
+    }
+   
     }
 
     
