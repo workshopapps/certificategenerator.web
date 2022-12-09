253ad1cbc9f7bf1484747632e7bcf5da90bd0f27
Tolulope Babatunde <babsbright69@gmail.com>
2022-12-09 16:14:36 +0100

    resolved conflict

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -249,6 +249,11 @@ const Dashboard = () => {
     getEvents();
   };
 
+//GET USERNAME FROM LOCALSTORAGE
+const profileName = localStorage.getItem('userName');
+
+
+
   return (
     <>
       <div className="dashboard">
@@ -257,18 +262,23 @@ const Dashboard = () => {
             <span className="dashboard__profile-pic">
               <img src={file || profilePic} alt="brand-kit" />   
             </span>
+
             {/* <label htmlFor="file" className="dashboard__upload-label">
               <img src={Upload} alt="upload-icon" />
               <input type="file" id="file" accept="image/*" name="file" onChange={onFileChange}  />
             </label>
             <Button name="Update Brand Kit" onClick={onUpdate} /> */}
+
+           
+           
           </div>
+
           <div className="flexx">
             <div className="dashboard__align-start">
               <h3 className="dashboard__text">Welcome</h3>
-              <h2 className="dashboard__title">Team Headlight</h2>
+              <h2 className="dashboard__title">{profileName}</h2>
               <p className="dashboard__description">
-                Let’s do the Accounts for you, Get a summary of all the
+            Get a summary of all the
                 Certificates and Job done here
               </p>
               <div>
