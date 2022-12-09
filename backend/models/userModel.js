834117c570e52d6e52da1e430e91c0a235f237a9
Isaac Ubani <isaacchimdi@gmail.com>
2022-12-09 12:36:29 +0100

    modified:   backend/models/userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -58,15 +58,14 @@ const UserSchema = new mongoose.Schema({
 
 UserSchema.pre("save", async function (next) {
   try {
-    const { email, user, name } = this
     // Check if new user has existing profile
-    const existingProfile = await Profile.findOne({ user });
+    const existingProfile = await Profile.findOne({ user: this._id });
 
     // Don't create new profile if one already exists
     if (existingProfile) return next();
 
     // Create new profile if user has no profile
-    await Profile.create({ email, user, name });
+    await Profile.create({ email: this.email, user: this._id, name: this.name });
 
     next();
   } catch (error) {
