9589d94a5d7432312587ab95c21c131a25995e3e
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-10 01:40:36 +0100

    fix: resolve error for google email already in use

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -52,10 +52,8 @@ const userSignup = handleAsync(async (req, res, next) => {
     email = payload["email"];
 
     //check db if user already exists
-    if (await userExist(email)){
-      
-    }
-
+    if (await userExist(email))
+      throw createApiError("email already in use", 401);
 
     //if not create new user
     const newUser = new User({
@@ -152,7 +150,6 @@ const userLogin = handleAsync(async (req, res, next) => {
   const user = await User.findOne({ email });
 
   if (!user)
-
     throw createApiError("A user for this email could not be found!", 400);
 
   const isEqual = await bcrypt.compare(
