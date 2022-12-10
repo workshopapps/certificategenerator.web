a34814cf26ebb59e9c7a0ed3f23cf324067cdffa
Luqman Adeniyi <luqmanadeniyi97@gmail.com>
2022-12-10 09:25:22 +0100

    getting the items in the fetched array

--- frontend/src/pages/Dashboard/TableRow.jsx
+++ frontend/src/pages/Dashboard/TableRow.jsx
@@ -1,11 +1,55 @@
 import React, { useState } from 'react'
 import { useRef } from 'react';
 import { useEffect } from 'react';
+import axios from 'axios';
+import { BsCaretRightFill } from 'react-icons/bs';
 import { ReactComponent as ActionIcon } from "./assets/actionIcon.svg";
+import ViewModal from './Modal/ViewModal';
+import EditModal from './Modal/EditModal';
 import "./dashboard.style.scss"
 
 const TableRow = ({item, handleChangeCertificateStatus, handleDeleteCertificate, getUserCertificates }) => {
+  const [openOptions, setOpenOptions] = useState(false);
+  const [openStatus, setOpenStatus] = useState(false);
+  const [openViewModal, setOpenViewModal] = useState(false);
+  const [openEditModal, setOpenEditModal] = useState(false);
+  const [viewData, setViewData] = useState("")
+  const [editData, setEditData] = useState("")
+  const baseURL = "https://certgo.hng.tech/api";
+  const accessToken = JSON.parse(localStorage.getItem("userData")).token
   const drop = useRef()
+
+  const axiosPrivate = axios.create({
+    baseURL,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`
+    }
+  });
+  const viewCertificate = async (item) => {
+    // try {
+    //   const res = await axiosPrivate.get(`/certificates/${id}`);
+    //   console.log(res.data.data.certificate);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    setViewData(item);
+  }
+  const editCertificate = (item) => {
+      console.log(item);
+      setEditData(item);
+     
+   
+    // const res = await axiosPrivate.get(`/certificates/${id}`);
+  }
+  const handleViewModal = (id) => {
+    setOpenViewModal(true)
+    viewCertificate(id)
+  }
+  const handleEditModal = (id) => {
+    setOpenEditModal(true)
+    editCertificate(id)
+  }
   const handleDelete = async (id) => {
     await handleDeleteCertificate(id)
     // getUserCertificates()
@@ -19,11 +63,11 @@ const TableRow = ({item, handleChangeCertificateStatus, handleDeleteCertificate,
     setOpenOptions(!openOptions)
     getUserCertificates()
   }
-  const [openOptions, setOpenOptions] = useState(false);
   useEffect(() => {
     function handleClickOutside(event) {
       if (drop.current && !drop.current.contains(event.target)) {
         setOpenOptions(false)
+        setOpenStatus(false)
       }
     }
     // Bind the event listener
@@ -36,11 +80,7 @@ const TableRow = ({item, handleChangeCertificateStatus, handleDeleteCertificate,
   return (
     <tr>
       <td>{item.name}</td>
-      {item.status === "canceled" ? (
-        <td>
-          <button className="cancel">Canceled</button>
-        </td>
-      ) : item.status === "pending" ? (
+      {item.status === "pending" ? (
         <td>
           <button className="pending">Pending</button>
         </td>
@@ -53,19 +93,31 @@ const TableRow = ({item, handleChangeCertificateStatus, handleDeleteCertificate,
       {/* <td>{item.date}</td>
       <td>PDF</td> */}
       <td className="action">
-        <ActionIcon
-          className="action-icon"
-          onClick={() => setOpenOptions(!openOptions)}
-        />
+        <span 
+        className="action-icon"
+        onClick={() => setOpenOptions(!openOptions)}
+        >
+        <ActionIcon/> 
+        </span>
 
         {openOptions && (
           <ul ref={drop} className="action__overlay">
-            <li className="action__overlay--item">View</li>
-            <li className="action__overlay--item">Edit</li>
-            <li className="action__overlay--item" onClick={() => handleStatus(item._id, 'issued')}>Update status</li>
+            <li className="action__overlay--item" onClick={() => handleViewModal(item)}>View</li>
+            <li className="action__overlay--item" onClick={() => handleEditModal(item)}>Edit</li>
+            <li className="action__overlay--item status" onClick={() => setOpenStatus(!openStatus)}><div>Status <span><BsCaretRightFill/></span></div>
+              {openStatus && (
+                <ul>
+                  <li className='status__pending' onClick={() => handleStatus(item._id, 'pending')}>Pending</li>
+                  <li className='status__issued' onClick={() => handleStatus(item._id, 'issued')}>Issued</li>
+                  {/* <li className='status__cancel' onClick={() => handleStatus(item._id, 'canceled')}>Cancelled</li> */}
+                </ul>
+              )}
+            </li>
             <li className="action__overlay--item" onClick={() => handleDelete(item._id)}>Delete</li>
           </ul>
         )}
+        <ViewModal open={openViewModal} viewData={viewData} onClose={() => setOpenViewModal(false)}/>
+        <EditModal open={openEditModal} editData={editData} getUserCertificates={getUserCertificates} onClose={() => setOpenEditModal(false)}/>
       </td>
     </tr>
   );
