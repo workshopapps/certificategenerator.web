de0160d00c105ab682c0962ac7e53b39510de2ce
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-10 13:39:26 +0100

    fix(BE): delete user on delete user profile

--- backend/models/profileModel.js
+++ backend/models/profileModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const User = require("./userModel");
 const { Schema } = mongoose;
 
 const ProfileSchema = new mongoose.Schema({
@@ -26,6 +27,17 @@ const ProfileSchema = new mongoose.Schema({
   },
   phoneNumber: {
     type: String
+  }
+});
+
+ProfileSchema.post("deleteOne", async function (next) {
+  try {
+    // Check if user has existing profile and delete
+    await User.findOneAndDelete({ _id: this.user.toString() });
+
+    next();
+  } catch (error) {
+    next(error);
   }
 });
 
