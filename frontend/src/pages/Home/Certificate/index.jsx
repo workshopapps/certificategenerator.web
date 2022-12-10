bccf8a0e7af6cb4182a8a1a2618a4c05c9c4d0e1
Gbolahan Oladeji <hormorgborlahan@yahoo.com>
2022-12-10 16:52:15 +0100

    Update index.jsx

--- frontend/src/pages/Home/Certificate/index.jsx
+++ frontend/src/pages/Home/Certificate/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import {ButtonLoader} from "../../../Component/";
+import { ButtonLoader } from "../../../Component/";
 import "./certificate.style.scss";
 import UploadCSV from "../../UploadCSV";
 import DatePicker from "react-datepicker";
