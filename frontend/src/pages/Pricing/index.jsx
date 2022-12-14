89c1c35c5d18ab2679b48fd9f2c336d013b62a4e
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-12-14 14:26:33 +0100

    chore: Pricing cleanup

--- frontend/src/pages/Pricing/index.jsx
+++ frontend/src/pages/Pricing/index.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./pricing.style.scss";
-import Feature from "./feature";
 import Slider from "./slider";
 import { data } from "./data";
+import Feature from "./feature";
 import "@splidejs/react-splide/css";
 import useAppProvider from "../../hooks/useAppProvider";
 import conatctLocatonIconStar from "../../assets/svgs/Conatct-locaton-icon-star.svg";
@@ -73,20 +73,20 @@ function Pricing({ amountHandler }) {
                   {sub.subType === "Standard" ? (
                     <div
                       style={{
+                        gap: "16px",
                         display: "flex",
                         alignItems: "center",
-                        gap: "16px",
                         marginBottom: "0.5rem"
                       }}
                     >
                       <h4>{sub.subType}</h4>
                       <div
                         style={{
-                          display: "flex",
-                          alignItems: "center",
                           gap: "6px",
+                          display: "flex",
                           padding: "6px 16px",
                           borderRadius: "12px",
+                          alignItems: "center",
                           backgroundColor: "#08372F1A"
                         }}
                       >
@@ -128,13 +128,6 @@ function Pricing({ amountHandler }) {
                       to={`${sub.subType !== "Basic" ? "/comingsoon" : ""}`}
                       style={
                         sub.subType === "Basic" ? { display: "none" } : {}
-                        // disabled && sub.subType === "Basic"
-                        //   ? {
-                        //       cursor: "not-allowed",
-                        //       backgroundColor: "#8ab9b2",
-                        //       pointerEvents: "none"
-                        //     }
-                        //   : {}
                       }
                       onClick={e =>
                         disabledHandler(
@@ -160,139 +153,16 @@ function Pricing({ amountHandler }) {
 
           <div className="sliderContainer">
             <Slider
-              value={value}
-              disabledHandler={disabledHandler}
               per={per}
+              value={value}
               access={access}
+              disabledHandler={disabledHandler}
             />
           </div>
         </article>
       </section>
 
       <Feature />
-
-      {/* <section>
-            <div className="testimonial">
-        <h1>Testimonials</h1>
-        <p className = 'testimonial-para'>What our users have to say about us</p>
-
-        <Splide
-          className="testimonial-wrapper"
-          options={{
-            gap: "20px",
-            perPage: 3,
-
-            breakpoints: {
-              1200: {
-                perPage: 2,
-              },
-              768: {
-                perPage: 1,
-              },
-            },
-
-            arrows: true,
-            pagination: true,
-            drag: "free",
-          }}
-        >
-          <SplideSlide>
-            <div className="testimonial-boxn test">
-              <div className="testimonial-flex">
-                <img src={person_1} alt="user" />
-                <div className="text">
-                  <p className="testimonial-name">Lindsay Favazza</p>
-                  <p className="testimonial-state">United States</p>
-                </div>
-              </div>
-              <p className="testimonial-test">
-                “Certgo is an incredible website for designing certificates! A
-                huge selection of templates, fonts, and colors; endless choices
-                at the tip of your fingers; easy editing and sending/sharing.
-                Best certificate generator I've used for a long time. If you
-                haven't tried it... try it! Victor Hayworth”
-              </p>
-            </div>
-          </SplideSlide>
-
-          <SplideSlide>
-            <div className="testimonial-boxn">
-              <div className="testimonial-flex">
-                <img src={person_2} alt="user" />
-                <div className="text">
-                  <p className="testimonial-name">Valerie Tan</p>
-                  <p className="testimonial-state">Singapore</p>
-                </div>
-              </div>
-              <p className="testimonial-test">
-                “Certgo is a fantastic piece of software! It saves us a lot of
-                time, as we were able to generate bulk certificates for our
-                students automatically. The customer support is also excellent
-                and helps us navigate the software and recommend a solution for
-                the changes we need.”
-              </p>
-            </div>
-          </SplideSlide>
-
-          <SplideSlide>
-            <div className="testimonial-boxn">
-              <div className="testimonial-flex">
-                <img src={person_3} alt="user" />
-                <div className="text">
-                  <p className="testimonial-name">Lea Botha</p>
-                  <p className="testimonial-state">South Africa</p>
-                </div>
-              </div>
-              <p className="testimonial-test">
-                “Certgo is great for designing certificates for my students
-                enrolled in my courses. The various letter font styles and font
-                colors are wonderful to choose from. The final look and design
-                of the certification are professional. I enjoy using this easy
-                website.”
-              </p>
-            </div>
-          </SplideSlide>
-
-          <SplideSlide>
-            <div className="testimonial-boxn">
-              <div className="testimonial-flex test-image2">
-                <img src={person_1} alt="user" />
-                <div className="text">
-                  <p className="testimonial-name">Jessica Anoke</p>
-                  <p className="testimonial-state">Nigeria</p>
-                </div>
-              </div>
-              <p className="testimonial-test">
-                “Easy, simple, and fast. The ease of use is really commendable.
-                Setup and configuration is quite fast and simple. I found it
-                really handy in sending out digital certificates for our
-                bootcamp. The certification process has become seamless with
-                only 3 steps to take in order to issue the credentials.”
-              </p>
-            </div>
-          </SplideSlide>
-
-          <SplideSlide>
-            <div className="testimonial-boxn">
-              <div className="testimonial-flex test-image">
-                <img src={person_5} alt="user" />
-                <div className="text">
-                  <p className="testimonial-name">Todd Holgate</p>
-                  <p className="testimonial-state">Poland</p>
-                </div>
-              </div>
-              <p className="testimonial-test">
-                “I came across this website when I needed a bulk certificate
-                generator for my course. It is straightforward and I like the
-                frames as they look very professional. It is quite affordable.”
-              </p>
-            </div>
-          </SplideSlide>
-        </Splide>
-      </div>
-            </section> */}
-
-      {/* <section className="bg"></section> */}
     </main>
   );
 }
